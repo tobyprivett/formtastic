d3b75d88da0f1e6401234454408448f1c6941c93
Toby Privett <toby@snaplab.co.uk>
2011-05-18 09:57:26 -0500

    Enabling unescaped html in :hint text

--- lib/formtastic/inputs/base/hints.rb
+++ lib/formtastic/inputs/base/hints.rb
@@ -2,17 +2,17 @@ module Formtastic
   module Inputs
     module Base
       module Hints
-        
+
         def hint_html
           if hint?
             template.content_tag(
-              :p, 
-              Formtastic::Util.html_safe(hint_text), 
+              :p,
+              hint_text,
               :class => (options[:hint_class] || builder.default_hint_class)
             )
           end
         end
-        
+
         def hint?
           !hint_text.blank? && !hint_text.kind_of?(Hash)
         end
@@ -20,7 +20,7 @@ module Formtastic
         def hint_text
           localized_string(method, options[:hint], :hint)
         end
-        
+
         def hint_text_from_options
           options[:hint]
         end
